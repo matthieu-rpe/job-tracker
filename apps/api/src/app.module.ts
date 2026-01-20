bda18ecf1febcf5eba1041eef636e3a73c7f4635
Matthieu RAPENNE <m.rapenne@hazelio.com>
2026-01-20 17:17:25 +0100

    refactor(api): Remove default app.service and app.controller

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
