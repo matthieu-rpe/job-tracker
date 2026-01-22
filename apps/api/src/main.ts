3c69463f8e87e32d223296fa334fb7f671530387
Matthieu RAPENNE <m.rapenne@hazelio.com>
2026-01-22 11:42:02 +0100

    docs(api): Documentation main.ts lifecycle

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -14,15 +14,47 @@ async function bootstrap() {
     defaultVersion: '1',
   });
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
+  /*
+   * FIRST : MIDDLEWARE
+   */
+
+  /*
+   * SECOND : GUARDS
+   */
+
+  /*
+   * THIRD + FIVE (res) : INTERCEPTORS
+   *
+   * Inspirated by the Aspect Oriented Programming (AOP) technique.
+   * - bind extra logic before / after method execution
+   * - transform the result returned from a function
+   * - transform the exception thrown from a function
+   * - extend the basic function behavior
+   * - completely override a function depending on specific conditions (e.g. for caching purpose)
+   */
+
+  // Serializer for every class (new keyword that creates a prototype different from Object.prototype) that exclude all fields excepts one with @Expose
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector), {
+      strategy: 'excludeAll',
     }),
   );
 
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  /*
+   * FOUR : PIPES
+   *
+   * Transformation : Transform input data to the desired form (e.g. from string to integer)
+   * Validation : evaluate input data and if unvalid, throw an exception
+   */
+
+  // For each route with a DTO : transform and validate the DTO
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
